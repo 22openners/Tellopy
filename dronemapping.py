e15c77c205199c7f3b15624b6e2e0c286c22e228
22openners <73253906+22openners@users.noreply.github.com>
2021-05-28 15:08:42 -0700

    Copied latest version of dronemapping

--- dronemapping.py
+++ dronemapping.py
@@ -1,57 +1,105 @@
-from djitellopy import tello
+from djitellopy import Tello
 import KeyPressModule as kp
-from time import sleep
 import cv2
+import numpy as np
+import time
+import math
+
+############ PARAMETERS ############
+fSpeed = 112  # Forward/Backward cm/sec
+aSpeed = 26  # Angular Velocity, Degrees/sec
+uSpeed = 26  # Up/Down cm/sec
+interval = 0.25
+
+dInterval = fSpeed * interval  # Distance Interval
+aInterval = aSpeed * interval  # Angular Interval
+hInterval = uSpeed * interval  # Height Interval
+#########################################
+x, y = 500, 500
+a = 0
+yaw = 0
 
 kp.init()
+tello = Tello()
+# tello.connect()
+# print(tello.get_battery())
+# global img
+# tello.streamon()
 
-drone = tello.Tello()
 
-drone.streamon()
-
-drone.connect()
-
-print(drone.get_battery())
-#Gets the Keyboard input
 def getKeyboardInput():
 
     lr, fb, ud, yv = 0, 0, 0, 0
-
     speed = 50
+    global x, y, yaw, a
+    d = 0
 
-    if kp.getKey("LEFT"): lr = -speed
+    if kp.getKey("LEFT"):
+        lr = -speed
+        d = dInterval
+        a = -180
 
-    elif kp.getKey("RIGHT"): lr = speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
+        d = -dInterval
+        a = 180
 
-    if kp.getKey("UP"): fb = speed
+    if kp.getKey("UP"):
+        fb = speed
+        d = dInterval
+        a = 270
 
-    elif kp.getKey("DOWN"): fb = -speed
+    elif kp.getKey("DOWN"):
+        fb = -speed
+        d = -dInterval
+        a = -90
 
     if kp.getKey("w"): ud = speed
-
     elif kp.getKey("s"): ud = -speed
 
-    if kp.getKey("a"):yv = -speed
+    if kp.getKey("a"):
+        yv = -speed
+        yaw += aInterval
 
-    elif kp.getKey("d"): yv = speed
+    elif kp.getKey("d"):
+        yv = speed
+        yaw -= aInterval
 
-    if kp.getKey("q"): drone.land(); sleep(3)
+    if kp.getKey("q"):
+        tello.land()
+        time.sleep(3)
+    if kp.getKey("e"): tello.takeoff()
 
-    if kp.getKey("e"): drone.takeoff()
+    if kp.getKey("z"):
+        cv2.imwrite(f'Resources/Images/{time.time()}.jpg', img)
+        time.sleep(0.3)
 
-    return [lr, fb, ud, yv]
+    a += yaw
+    x += int(d*math.cos(math.radians(a)))
+    y += int(d*math.sin(math.radians(a)))
+
+
+    return [lr, fb, ud, yv, x, y]
+
+
+def drawPoints():
+    cv2.circle(img, (points[0], points[1]), 20, (0, 0, 255), cv2.FILLED)
+
 
 while True:
 
     vals = getKeyboardInput()
 
-    drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    tello.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
-    sleep(0.05)
+    img = np.zeros((1000, 1000, 3), np.uint8)
+    points = (vals[4], vals[5])
+    drawPoints(img, points)
 
-#Stream Window
-while True:
-  img = drone.get.frame_read().frame
-  img = cv2.resize(img, (360, 240))
-  cv2.imshow("image", img)
-  cv2.waitKey(1)
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
+
+# img = tello.get_frame_read().frame
+# img = cv2.resize(img, (360, 240))
+# cv2.imshow("image", img)
+# cv2.waitKey(1)5)
